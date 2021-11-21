fa9c4dbbb9db533ad948cedbe5d2da261516117d
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-21 12:12:33 +0300

    Using colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Hello Martin"),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text("Hello Coders"),
-    ),
-    bottomNavigationBar: BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      child: Container(height: 50.0),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () =>{},
-      tooltip: 'Increment Counter',
-      child: const Icon(Icons.add),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-  ),
-));
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello Martin"),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: const Center(
+          child: Text(
+            "Hello Coders",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlower'
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 50.0),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          tooltip: 'Increment Counter',
+          backgroundColor: Colors.red[600],
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    ));
