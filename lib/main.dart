ef0eae3ca8e262ce3d476cc4f15d8b83d5b004ad
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-23 22:24:23 +0300

    coverting stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
